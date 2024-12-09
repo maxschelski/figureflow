46573246180f78078c3514e13054ed9c6aabeb53
Max Schelski <max.schelski@googlemail.com>
2024-12-09 11:08:31 +0100

    Small: plot line groups alpha

--- figureflow/plots/line_plot.py
+++ figureflow/plots/line_plot.py
@@ -1,11 +1,20 @@
 import seaborn as sns
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
 class LinePlot():
     CONTINUOUS_X = True
 
     def __init__(self, x, y, hue, data, hue_order, plot_colors, ax,
-                 x_range, ci=95, **kwargs):
+                 x_range, ci=95, plot_line_group_cols=None,
+                 plot_line_group_alpha=0.1,
+                 **kwargs):
+        """
+
+        :param plot_line_group_cols: List of columns for which unique combinations
+            (only for plot_type == line)
+        """
         data[x] = pd.to_numeric(data[x])
         data.sort_values(x, inplace=True)
 
@@ -16,6 +25,8 @@ class LinePlot():
         self.ax = ax
         self.x_range = x_range
         self.ci = ci
+        self.plot_line_group_cols = plot_line_group_cols
+        self.plot_line_group_alpha = plot_line_group_alpha
 
         if type(hue) == type(None):
             nb_colors = 1
@@ -39,7 +50,7 @@ class LinePlot():
                 data=self.data, variables=self.variables,
                 estimator="mean", ci=self.ci, n_boot=1000, seed=None,
                 sort=None, err_style="band", err_kws=None, legend="full")
-        
+
         plot.map_hue(palette=self.plot_colors, order=self.hue_order, norm=None)
         
         plot._attach(self.ax)
@@ -55,5 +66,24 @@ class LinePlot():
         else:
             kws = {}
 
+        if self.plot_line_group_cols is not None:
+            groups = self.data[self.plot_line_group_cols].drop_duplicates()
+
+            for group in groups.values:
+                # exclude groups where all group vals are NaN
+                all_groups_nan = True
+                for group_val in group:
+                    if not np.isnan(group_val):
+                        all_groups_nan=False
+                        break
+                if all_groups_nan:
+                    continue
+                plot_data = self.data.set_index(self.plot_line_group_cols)
+                plot_data = plot_data.loc[tuple(group)]
+                self.ax.plot(plot_data[self.variables["x"]],
+                             plot_data[self.variables["y"]],
+                             color="Silver", alpha=self.plot_line_group_alpha)
+
         plot.plot(self.ax,kws)
+
         return plot, []
