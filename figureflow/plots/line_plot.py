fa1a019010dd2075717a704f7c1dd63d76808df8
Max Schelski <max.schelski@googlemail.com>
2025-02-04 08:09:45 +0100

    hue dashed lines

--- figureflow/plots/line_plot.py
+++ figureflow/plots/line_plot.py
@@ -8,7 +8,8 @@ class LinePlot():
 
     def __init__(self, x, y, hue, data, hue_order, plot_colors, ax,
                  x_range, ci=95, plot_line_group_cols=None,
-                 plot_line_group_alpha=0.1,
+                 plot_line_group_alpha=0.1, dashes=None,
+                 use_dashes_for_hue=False,
                  **kwargs):
         """
 
@@ -27,6 +28,8 @@ class LinePlot():
         self.ci = ci
         self.plot_line_group_cols = plot_line_group_cols
         self.plot_line_group_alpha = plot_line_group_alpha
+        self.dashes = dashes
+        self.use_dashes_for_hue = use_dashes_for_hue
 
         if type(hue) == type(None):
             nb_colors = 1
@@ -42,16 +45,25 @@ class LinePlot():
         self.variables["y"] = y
         if hue is not None:
             self.variables["hue"] = hue
+            if self.use_dashes_for_hue:
+                self.variables["style"] = hue
+                if self.dashes is None:
+                    self.dashes = True
 
     def plot(self):
         # sns.lineplot(data=self.data, x=self.x, y=self.variables["y"],
         #              ax=self.ax)
+
         plot = sns.relational._LinePlotter(
                 data=self.data, variables=self.variables,
                 estimator="mean", ci=self.ci, n_boot=1000, seed=None,
                 sort=None, err_style="band", err_kws=None, legend="full")
 
         plot.map_hue(palette=self.plot_colors, order=self.hue_order, norm=None)
+
+        if self.use_dashes_for_hue:
+            plot.map_style(markers=None, dashes=self.dashes,
+                           order=self.hue_order)
         
         plot._attach(self.ax)
 
@@ -68,21 +80,22 @@ class LinePlot():
 
         if self.plot_line_group_cols is not None:
             groups = self.data[self.plot_line_group_cols].drop_duplicates()
+            if len(groups) > 1:
+                for group in groups.values:
+                    # exclude groups where all group vals are NaN
+                    all_groups_nan = True
+                    for group_val in group:
+                        if not np.isnan(group_val):
+                            all_groups_nan=False
+                            break
+                    if all_groups_nan:
+                        continue
+                    plot_data = self.data.set_index(self.plot_line_group_cols)
+                    plot_data = plot_data.loc[tuple(group)]
 
-            for group in groups.values:
-                # exclude groups where all group vals are NaN
-                all_groups_nan = True
-                for group_val in group:
-                    if not np.isnan(group_val):
-                        all_groups_nan=False
-                        break
-                if all_groups_nan:
-                    continue
-                plot_data = self.data.set_index(self.plot_line_group_cols)
-                plot_data = plot_data.loc[tuple(group)]
-                self.ax.plot(plot_data[self.variables["x"]],
-                             plot_data[self.variables["y"]],
-                             color="Silver", alpha=self.plot_line_group_alpha)
+                    self.ax.plot(plot_data[self.variables["x"]],
+                                 plot_data[self.variables["y"]],
+                                 color="Silver", alpha=self.plot_line_group_alpha)
 
         plot.plot(self.ax,kws)
 
